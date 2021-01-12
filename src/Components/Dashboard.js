387e1874d75a600c26eaefb4d8eba62f0304af34
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-01-12 13:12:23 +0100

    created db right footer, incl. pagination and download btns

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './Dashboard.css';
 import { FiSearch } from 'react-icons/fi';
+import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosCloudDownload } from 'react-icons/io';
 import allUsers from '../usersData/users';
 import UsersButtons from './UsersButtons';
 import UsersList from './UsersList';
@@ -33,12 +36,14 @@ function Dashboard() {
                     <h3>Hello, <span> Nurudeen </span></h3>
                     <p>Welcome to your dashboard, kindly sort through the user base</p>
                 </div>
+
                 <form>
                     <div className="form-control">
                         <FiSearch className="form-control__searchIcon"/>
                         <input type="text" placeholder="Find a user"/>
                     </div>
                 </form>
+
                 <div className="dashboard__left-users">
                     <h3>Show users</h3>
                     <UsersButtons genders={genders} filterUsers={filterUsers}/>
@@ -48,16 +53,23 @@ function Dashboard() {
             <div className="dashboard__right">
                 <h3>{activeGenderList}</h3>
                 <p>Filter by</p>
+
                 <form>
                     <div className="form-control-2">
                         <FiSearch className="form-control-2__searchIcon"/>
                         <input type="text" placeholder="Find in list"/>
                     </div>
                 </form>
-               <UsersList users={users} />
-               <div className="dashboard__right-footer">
 
-               </div>
+                <UsersList users={users} />
+
+                <div className="dashboard__right-footer">
+                   <button className="list__download-btn"> <IoIosCloudDownload className="download-icon"/> Download Results </button>
+
+                   <div className="list__pagination-btns">  <button className="pagination__btn"> <IoIosArrowBack /> </button>
+                   <button className="pagination__btn">  <IoIosArrowForward /> </button> </div>  
+                </div>
+
             </div>
             
         </div>
