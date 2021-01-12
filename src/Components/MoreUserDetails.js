f554f2302ab76a75d0e5fb77ae132397b405a4d6
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-01-12 16:36:00 +0100

    chopped off unnecessary part of user joined date

--- src/Components/MoreUserDetails.js
+++ src/Components/MoreUserDetails.js
@@ -20,7 +20,7 @@ function MoreUserDetails ({title, firstName, lastName, streetNum, streetName, ci
                     <h3 className="user__name">{title} {firstName} {lastName} <span>{age}</span> </h3>
                     <p className="user__address">{streetNum} {streetName}, {city}, {state} </p>
                     <p className="user__email"> <BiEnvelope /> {email}</p>
-                    <p className="user__registered">JOINED: {registered}</p>
+                    <p className="user__registered"> JOINED: {registered.slice(0, 10)}</p>
                     <p className="user__phone"> <FiPhoneCall /> {phone}</p>
                     <p className="user__cell"> <BsPhone /> {cell}</p>
                 </div>
